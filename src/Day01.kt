d7924fe1e8f8ec29f3d17e817311c40d1a09fa7d
Martin Tarjányi <tmartin193@gmail.com>
2021-12-05 00:15:26 +0100

    Use inner function for easier separation

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,4 +1,14 @@
 fun main() {
+    fun part1(input: List<String>): Int {
+        return input.windowed(2).count { (previous, next) -> next > previous}
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.windowed(size = 3)
+            .windowed(size = 2)
+            .count {(previousGroup, nextGroup) -> nextGroup.sumOf { it.toInt() } > previousGroup.sumOf { it.toInt() }}
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
@@ -7,14 +17,4 @@ fun main() {
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
-}
-
-fun part1(input: List<String>): Int {
-    return input.windowed(2).count { (previous, next) -> next > previous}
-}
-
-fun part2(input: List<String>): Int {
-    return input.windowed(size = 3)
-        .windowed(size = 2)
-        .count {(previousGroup, nextGroup) -> nextGroup.sumOf { it.toInt() } > previousGroup.sumOf { it.toInt() }}
 }
