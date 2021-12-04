2a9925eaac9ae9ce3e94c59128fc44ddf4c35100
Martin Tarjányi <tmartin193@gmail.com>
2021-12-05 00:10:42 +0100

    Solve Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,20 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    return input.windowed(2).count { (previous, next) -> next > previous}
+}
+
+fun part2(input: List<String>): Int {
+    return input.windowed(size = 3)
+        .windowed(size = 2)
+        .count {(previousGroup, nextGroup) -> nextGroup.sumOf { it.toInt() } > previousGroup.sumOf { it.toInt() }}
 }
