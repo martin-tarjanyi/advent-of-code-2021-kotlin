241f787ca4314460cb3553b3959daaf964a36922
Martin Tarjányi <tmartin193@gmail.com>
2021-12-13 19:49:54 +0100

    Solve Day 7

---
+++ src/Day07.kt
@@ -0,0 +1,42 @@
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val numbers = input.first().split(",").map { it.toInt() }
+        val median = numbers.median()
+        return numbers.sumOf { abs(median - it) }
+    }
+
+    fun part2(input: List<String>): Int {
+        val numbers = input.first().split(",").map { it.toInt() }
+        val max = numbers.maxOrNull() ?: throw IllegalArgumentException("Empty input.")
+
+        return (0..max).minOf { alignPosition ->
+            numbers.sumOf { num -> exponentialFuelCost(num, alignPosition) }
+        }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day07_test")
+    check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
+
+    val input = readInput("Day07")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun List<Int>.median(): Int = if (this.size % 2 == 1) {
+    val medianIndex = this.size / 2
+    this.sorted()[medianIndex]
+} else {
+    val lowerMedianIndex = this.size / 2 - 1
+    val upperMedianIndex = this.size / 2
+    val sorted = this.sorted()
+    (sorted[upperMedianIndex] + sorted[lowerMedianIndex]) / 2
+}
+
+fun exponentialFuelCost(num1: Int, num2: Int): Int {
+    val distance = abs(num1 - num2)
+    return ((distance + 1) / 2.0 * distance).toInt()
+}
